4f0374c7b28ee87f5d8bf0a0c6249e17eee64931
MitchellMarkGeorge <mitchellmmarkgeorge@gmail.com>
2019-09-28 20:09:04 -0400

    inital commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import * as path from 'path'
+
+let app = express();
+
+let port = 3000 || process.env;
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`server is runnig at port ${port}`);
+    //console.log(path.resolve('public', 'index.html'));
+});
